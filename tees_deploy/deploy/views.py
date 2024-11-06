5698ef2aeaeedd516cd739080027285ac6ec71d0
TUXSEAL <50582514+TUXSEAL@users.noreply.github.com>
2024-11-05 23:55:18 -0300

    Acton

--- tees_deploy/deploy/views.py
+++ tees_deploy/deploy/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 def index(request):
     return HttpResponse("INDEX")
 
 def update(request):
+    os.system("git pull origin main")
     return HttpResponse("UPDATE")
